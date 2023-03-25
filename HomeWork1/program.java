58c2fad17bf70f10cd7e3e7226a6dfea148c1d96
AntonAntonkin <antonkin_anton@mail.ru>
2023-03-25 21:49:13 +0500

    task1 done

---
+++ HomeWork1/program.java
@@ -0,0 +1,8 @@
+package HomeWork1;
+
+public class program {
+
+    public static void main(String[] args) {
+        task1.triangularNumber();
+    }
+}
---
+++ HomeWork1/task1.java
@@ -0,0 +1,23 @@
+package HomeWork1;
+import java.util.Scanner;
+
+public class task1 {
+
+    public static void triangularNumber() {
+        System.out.println("Введите число:");
+        var n = new Scanner(System.in).nextInt();
+
+        System.out.printf("\nТреугольное число %d = ", n);
+        System.out.println(n * (n + 1) / 2 + "\n");
+        System.out.printf("Нарисовать треугольник с основанием %d?:\n 1 - Да\n", n);
+
+        if (new Scanner(System.in).next().equals("1")) {
+            for (int i = 1; i <= n; i++) {
+                System.out.print(new String(new char[n - i]).replace('\0', ' '));
+                System.out.print(new String(new char[i]).replace("\0", "* "));
+                System.out.println();
+            } 
+        }
+        System.out.println("\nДо скорой встречи!");
+    }
+}
